85345133fc32b7e093f7e7c61e42f34dcb47cf2e
OleksPal <oleksandr.palamarchuk1310@gmail.com>
2021-11-05 17:02:35 +0200

    The producer-consumer problem v1.0

---
+++ Producer-ConsumerProblem/Buffer.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+
+namespace Producer_ConsumerProblem
+{
+    class Buffer
+    {
+        public Semaphore s; // Mutual exclusion between processes
+        public Semaphore e; // Defining empty space
+        public Semaphore f; // Defining the space that is filled by the producer
+
+        public Buffer()
+        {
+            s = new(1, 1);
+            e = new(1, 1);
+            f = new(0, 1);
+        }
+    }
+}
--- Producer-ConsumerProblem/Program.cs
+++ Producer-ConsumerProblem/Program.cs
@@ -1,12 +1,60 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Producer_ConsumerProblem
 {
     class Program
     {
+        static Buffer buf = new();
         static void Main(string[] args)
+        {            
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Producer: ");
+                Producer();
+                Console.WriteLine("\nConsumer: ");
+                Consumer();
+                Console.WriteLine();
+            }
+        }
+
+        static void Producer()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Producing something");
+            Task.Delay(1000);
+
+            Console.WriteLine("Blocking process");
+            buf.s.WaitOne();
+
+            Console.WriteLine("Decrease the value of the empty space in the buffer");
+            buf.e.WaitOne();
+
+            Console.WriteLine("Appending the newly produced data in the buffer");
+            Task.Delay(1000);            
+
+            Console.WriteLine("Marking buffer as full");
+            buf.f.Release();
+
+            Console.WriteLine("Releasing process");
+            buf.s.Release();
+        }
+
+        static void Consumer()
+        {
+            Console.WriteLine("Blocking process");
+            buf.s.WaitOne();
+
+            Console.WriteLine("Marking buffer as empty");
+            buf.f.WaitOne();
+
+            Console.WriteLine("Taking the data from the buffer");
+            Task.Delay(1000);            
+
+            Console.WriteLine("Increase the value of the empty space in the buffer");
+            buf.e.Release();
+
+            Console.WriteLine("Releasing process");
+            buf.s.Release();
         }
     }
 }
