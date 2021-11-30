44e32c1c90715850223160f491b8b2fea77dbe0d
OleksPal <oleksandr.palamarchuk1310@gmail.com>
2021-11-30 15:37:02 +0200

    Print symbols by threads

---
+++ SimpleThreadProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace SimpleThreadProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                new Thread(() => PrintSymbols(3, 'a')).Start();
+                new Thread(() => PrintSymbols(5, 'b')).Start();
+            }
+        }
+
+        static void PrintSymbols(int number, char symbol)
+        {
+            for (int i = 0; i < number; i++)
+                Console.Write(symbol);
+        }
+    }
+}
